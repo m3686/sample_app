74f5ccffdde7e011665b67fbf7cfb54c6438e9da
Naoki Bizen <naoki.bizen@appliplanet.co.jp>
2011-01-02 20:24:55 +0900

    Added spork, autotest (autotest need to be fixed)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
  # Return a title on a per-page basis.
  def title
- base_title = "RoR Tutorials Sample App"
+ base_title = "RoR Tutorial Sample App"
 		 if @title.nil?
 		    base_title
 		 else
